e5fafa86dd43c0e339745e22a0f2fa5e3625513e
azesm <azesm@yahoo.com>
2024-03-15 12:42:03 +0300

    part1_ 5, 21.20 404.png

--- vendor/wfm/ErrorHandler.php
+++ vendor/wfm/ErrorHandler.php
@@ -13,6 +13,27 @@ class ErrorHandler
             error_reporting(0);
         }
         set_exception_handler([$this, 'exceptionHandler']);
+        set_error_handler([$this, 'errorHandler']);
+        ob_start();
+        register_shutdown_function([$this, 'fatalErrorHandler']);
+    }
+
+    public function errorHandler($errno, $errstr, $errfile, $errline)
+    {
+        $this->logError($errstr, $errfile, $errline);
+        $this->displayError($errno, $errstr, $errfile, $errline);
+    }
+
+    public function fatalErrorHendler()
+    {
+        $error = error_get_last();
+        if (!empty($error) && $error['type'] & (E_ERROR | E_PARSE | E_COMPILE_ERROR | E_CORE_ERROR)) {
+            $this->logError($error['message'], $error['file'], $error['line']);
+            ob_end_clean();
+            $this->displayError($error['type'], $error['message'], $error['file'], $error['line']);
+        } else {
+            ob_end_flush();
+        }
 
     }
 
@@ -25,23 +46,36 @@ class ErrorHandler
     }
 
 
-    protected function logErrors ($message = '', $file = '', $line = '')
+    protected function logError($message = '', $file = '', $line = '')
     {
 
-        file_put_contents(LOGS)
+        file_put_contents(
+            LOGS . '/errors.log',
+        "[" . date('Y-m-d H:i:s') . "] Текст ошибки: {$message} | Файл: {$file} | Строка: {$line}\n=================\n",
+            FILE_APPEND);
 
     }
 
-    protected function logError ($message = '', $file = '', $line = '')
-    {
-
-
-    }
 
     protected function displayError ($errno, $errstr, $errfile, $errline, $responce = 500)
     {
 
 
+        if ($responce == 0) {
+            $responce = 404;
+        }
+        http_response_code($responce);
+        if ($responce == 400 && !DEBUG) {
+            require_once WWW . '/errors/404.php';
+            die;
+        }
+        if (DEBUG) {
+            require_once WWW . '/errors/development.php';
+
+        } else {
+            require_once WWW . '/errors/production.php';
+        }
+        die;
     }
 
 }
